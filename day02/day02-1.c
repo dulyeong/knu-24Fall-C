ad1edd078f63b26ee1cd68088f6ec4b33aeae9cd
dulyeong <104970793+dulyeong@users.noreply.github.com>
2024-09-12 16:45:03 +0900

    double

--- day02/day02-1.c
+++ day02/day02-1.c
@@ -2,7 +2,7 @@
 
 int main() {
 	int way = 0;
-	float x, y=0;
+	double x, y=0;
 	printf("원하는 기능을 입력하시오.\n");
 	printf("1.더하기 2.빼기 3.곱하기 4.나누기\n");
 	printf("기능 : ");
@@ -15,10 +15,10 @@ int main() {
 	if (way == 1)
 		printf("%lf + %lf = %lf\n", x, y, x + y);
 	else if (way == 2)
-		printf("%lf - %lf = %lf", x, y, x - y);
+		printf("%lf - %lf = %lf\n", x, y, x - y);
 	else if (way == 3)
-		printf("%lf * %lf = %lf", x, y, x * y);
+		printf("%lf * %lf = %lf\n", x, y, x * y);
 	else
-		printf("%lf / %lf = %lf", x, y, x / y);
+		printf("%lf / %lf = %lf\n", x, y, x / y);
 	return 0;
 }
