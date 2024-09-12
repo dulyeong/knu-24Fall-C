373e26d2458527dde236bc1f423d7710ef911014
dulyeong <104970793+dulyeong@users.noreply.github.com>
2024-09-12 16:30:06 +0900

    new

---
+++ day02/day02-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	int way = 0;
+	float x, y=0;
+	printf("원하는 기능을 입력하시오.\n");
+	printf("1.더하기 2.빼기 3.곱하기 4.나누기\n");
+	printf("기능 : ");
+	scanf_s("%d", &way);
+	printf("숫자 1을 입력하세요 :");
+	scanf_s("%lf", &x);
+	printf("숫자 2을 입력하세요 :");
+	scanf_s("%lf",&y);
+
+	if (way == 1)
+		printf("%lf + %lf = %lf\n", x, y, x + y);
+	else if (way == 2)
+		printf("%lf - %lf = %lf", x, y, x - y);
+	else if (way == 3)
+		printf("%lf * %lf = %lf", x, y, x * y);
+	else
+		printf("%lf / %lf = %lf", x, y, x / y);
+	return 0;
+}
