d70470cf0a5406b8e924ccabdcbd4e56966b37cb
dulyeong <104970793+dulyeong@users.noreply.github.com>
2024-09-26 15:55:59 +0900

    .

--- day04/day04-2.c
+++ day04/day04-2.c
@@ -4,9 +4,9 @@ void swap(int* a,int *b);
 
 int main() {
 	int num1=10, num2=20;
-	printf("%d %d", num1,num2);
+	printf("%d %d\n", num1,num2);
 	swap(&num1, &num2);
-	printf("%d %d", num1, num2);
+	printf("%d %d\n", num1, num2);
 	return 0;
 }
 
