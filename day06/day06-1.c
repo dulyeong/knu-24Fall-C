c086dc5188e97e969429288694609e7b21868ad3
dulyeong <104970793+dulyeong@users.noreply.github.com>
2024-10-07 14:17:07 +0900

    보충

---
+++ day06/day06-1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct NODE {
+	int data;
+	struct NODE *link;
+};
+
+struct NODE* create_node(int data) {
+	struct NODE* new_node = (struct NODE*)malloc(sizeof(struct NODE));
+	new_node->data = data;
+	new_node->link = NULL;
+
+	return new_node;
+}
+
+struct NODE* last_node() {
+	struct NODE* cur = head;
+	while (cur->link != NULL)
+	{
+		cur = cur->link;
+	}
+	return cur;
+}
+
+void insert_node_last(struct NODE* new_node) {
+	struct NODE* last = last_node();
+	last->link = new_node;
+}
+
+void print_nodes() {
+	struct NODE* cur = head->link;
+	while (cur!=NULL)
+	{
+		printf("%d\n", cur->data);
+		cur = cur->link;
+	}
+}
+
+struct NODE* find_node(int value) {
+	struct NODE* cur = head->link;
+	while (cur !=NULL)
+	{
+		if (cur->data == value) return cur;
+		cur = cur->link;
+	}
+	return NULL;
+}
+
+int delete_node(int value) {
+	struct NODE* prev = head;
+	struct NODE* cur = head->link;
+
+}
+
+struct NODE* head;
+
+int main() {
+
+	head = (struct NODE*)malloc(sizeof(struct NODE));
+	head->link = NULL;
+	insert_node_last(create_node(1));
+	return 0;
+}
