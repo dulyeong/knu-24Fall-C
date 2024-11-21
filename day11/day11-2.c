6237947d5b6eacf01f2548df24ca8b1e05cfb518
dulyeong <104970793+dulyeong@users.noreply.github.com>
2024-11-21 16:34:22 +0900

    .

--- day11/day11-2.c
+++ day11/day11-2.c
@@ -2,6 +2,13 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 //log10(x)+sin(x)
+
+float f(double x) { //확인
+	float res = log10(x) + sin(x);
+	return res;
+}
+
+
 int main() {
 	int start, end, area;
 	printf("목표 함수 : -log10(1/x)+sin(x) \n");
@@ -14,8 +21,8 @@ int main() {
 
 	printf("시행할 최대구간을 입력하세요(2^n) : ");
 	scanf_s("%d", &area);
-	float bottom = end - start;	//길이
-	float extent = 0;
+	double bottom = end - start;	//길이
+	double extent = 0;
 
 	
 	for (int i = 0; i < area; i++)
@@ -24,7 +31,8 @@ int main() {
 		for (int j = 1; j <= pow(2, i); j++)
 		{
 			extent += (bottom / pow(2, i)) * //밑변(길이 표현됨)
-			(log10(start + (bottom / pow(2, i))*j) + sin(start + (bottom / pow(2, i))*j));
+			//(log10(start + (bottom / pow(2, i))*j) + sin(start + (bottom / pow(2, i))*j));
+			f(start + (bottom / pow(2, i))*j);
 			//높이(점 표현됨)	
 		}
 		printf("구간 \t\t%.0f 적분 결과 : %lf\n", pow(2, i),extent);
