23c62a6b665f586aeeeb7a5fd3a12a1c6d3f7e1c
dulyeong <104970793+dulyeong@users.noreply.github.com>
2024-11-29 03:04:47 +0900

    Update day12-1.c

--- day12/day12-1.c
+++ day12/day12-1.c
@@ -4,52 +4,40 @@
 
 
 // topping_len은 배열 topping의 길이입니다.
-int solution(int topping[], int topping_len) {
+int solution(int topping[], size_t topping_len) {
     int count = 0;
-    int elder_cake[100000];   //철수가 가진 범위(케이크)
-    int younger_cake[100000];    //동생이 가진 범위
 
+    int elder[100001] = { 0 };  //인덱스로 종을 표현
+    int younger[100001] = { 0 };
+    int elder_topp = 0;   //종의 수
+    int younger_topp = 0;
 
-    for (int i = 0; i < topping_len; i++) {     //나누는 곳
-        printf("%d\n", topping_len);
-        int elder_topping = 0;  //토핑 종류
-        int younger_topping = 0;
-
-        for (int j = 0; j <= i; j++) {
-            elder_cake[j] = topping[j];
+    for (int i = 0; i < topping_len; i++) {     //토핑 종 도감 채우기, 토핑 종 세기
+        if (elder[topping[i]] == 0) {
+            elder_topp++;
         }
-        for (int j = topping_len; j > i; j--) {
-            younger_cake[j-1] = topping[j-1];
-        }
+        elder[topping[i]]++;    //종에 따른 개수
+    }
 
-        int topp_spec = 0;
-        while (topp_spec <= 10000) {
-            for (int j = 0; j < strlen(elder_cake); j++) {  //토핑갯수 세기
-                if (elder_cake[j] == topp_spec) {
-                    elder_topping++;
-                }
-            }
-            for (int j = 0; j <= strlen(younger_cake); j++) {
-                if (younger_cake[j] == topp_spec) {
-                    younger_topping++;
-                }
-            }
-            topp_spec++;
+
+    for (int i = 0; i < topping_len; i++) {
+        //동생 하나씩 추가
+        if (younger[topping[i]] == 0) {
+            younger_topp++;
+        }
+        younger[topping[i]]++;
+
+        //형 하나 씩 제거
+        elder[topping[i]]--;
+        if (elder[topping[i]] == 0) {
+            elder_topp--;
         }
 
-        if (elder_topping == younger_topping) {
-            printf("%d\n", i);
+
+        if (elder_topp == younger_topp) {
             count++;
         }
     }
-
     return count;
 }
 
-int main() {
-    int t[100000] = {1, 2, 1, 3, 1, 4, 1, 2};
-    
-    printf("%d", sizeof(t) / sizeof(int));
-    printf("%d",solution(t, sizeof(t) / sizeof(int)));
-
-}
